f87724cd3725f1b6a5a6b5e047cb5a2b22f1cd36
ETGuy36 <119773281+ETGuy36@users.noreply.github.com>
2023-02-16 21:13:43 -0600

    Update lock-in.py

--- sim/lock-in.py
+++ sim/lock-in.py
@@ -4,25 +4,32 @@ from scipy import signal
 
 np.random.seed(5)
 
-
+#This function creates a sine wave of specified value
 def sine_wave(freq, phase=0, fs=10000):
     rad = np.radians(phase)
     t = np.arange(0,fs) / fs
     return np.sin(2 * np.pi * freq * t + rad)
 
+#This function creates a sine wave of specified value
 def cos_wave(freq, phase=0, fs=10000):
     rad = (phase / 360) * (2.0 * np.pi)
     t = np.arange(0,fs) / fs
     return np.cos(2 * np.pi * freq * t + rad)
 
+#This function creates a low pass filter to pass signals through
 def bw_low_pass(fs, cutoff, order, data):
     ncf = cutoff / (0.5 * fs)
     b,a = signal.butter(order, ncf, btype='low', analog=False)
     return signal.filtfilt(b,a,data)
 
+#This function generates a noise of specified value
 def gen_noise(low=-1, high=1, size=10000):
     return np.clip(np.random.normal(0,1,size), low, high)
 
+""" 
+This function generates a sine wave and adds noise to the wave then plots the wave
+both before and after implementing the low pass filter. Different noise levels were also 
+incorporated """
 def show_lock_in():
     sine = sine_wave(100, fs=20000)
     noisy_sine = gen_noise(size=20000) + 0.1*sine_wave(100, fs=20000)
@@ -58,7 +65,8 @@ def show_lock_in():
     plt.tight_layout()
     plt.legend()
     plt.show()
-
+    
+#This function plots the phases of each sine wave with noise after passing it through a low pass filter
 def show_phase_sens():
     noisy_sine = gen_noise(size=10000) +0.1*sine_wave(100, fs=10000)
     noisy_135ph = noisy_sine* sine_wave(100, phase=135, fs=10000)
@@ -73,13 +81,12 @@ def show_phase_sens():
     plt.legend()
     plt.show()
 
-
-
+#runs the program
 def main():
     show_lock_in()
     #show_phase_sens()
 
-
+#Initializes main and runs program
 if __name__ == "__main__":
     main()
 
